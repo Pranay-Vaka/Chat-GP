71bf25f5676acfcf8baee8610898cd0ba7f6c6dc
Pranay-Vaka <vakapranaya05@gmail.com>
2024-02-12 14:59:35 +0000

    something

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
 import logging
 from fhirclient import client
 settings = {
-    'app_id': 'my_web_app',
-    'api_base': 'https://fhir-open-api-dstu2.smarthealthit.org'
+    'app_id': '3708f01857cc2d7864d6108f690a9e80',
+    'api_base': 'https://chatgp-server.fhir.azurehealthcareapis.com/'
 }
 smart = client.FHIRClient(settings=settings)
 
-import fhirclient.models.patient as p
-patient = p.Patient.read('hca-pat-1', smart.server)
-patient.birthDate.isostring
-# '1963-06-12'
-smart.human_name(patient.name[0])
-# 'Christy Ebert'
+# import fhirclient.models.patient as p
+# patient = p.Patient.read('hca-pat-1', smart.server)
+# birth_date = patient.birthDate.isostring
+# # '1963-06-12'
+# smart.human_name(patient.name[0])
+# # 'Christy Ebert'
